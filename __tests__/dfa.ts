2edc86ff0d4ead840928c98acdbd766bdac514f3
Bryan Terce <bryan.terce@gmail.com>
2020-04-18 11:07:49 -0700

    Add some tests

---
+++ __tests__/dfa.ts
@@ -0,0 +1,57 @@
+import { stripIndent } from "common-tags";
+import { parseDFA } from "../src/index";
+
+describe("invalid DFA", () => {
+  it("fails DFAs with an empty alphabet", () => {
+    const dfa = stripIndent`
+      alphabet: {}
+      start: Q0
+      accepting: {Q0}
+      
+      Q0 (a -> Q0)
+    `;
+    expect(() => parseDFA(dfa)).toThrow();
+  });
+
+  it("fails DFAs with empty accepting states", () => {
+    const dfa = stripIndent`
+      alphabet: {a}
+      start: Q0
+      accepting: {}
+      
+      Q0 (a -> Q0)
+    `;
+    expect(() => parseDFA(dfa)).toThrow();
+  });
+});
+
+describe("minimal DFA", () => {
+  const expected = {
+    alphabet: ["a"],
+    initialState: "Q0",
+    acceptingStates: ["Q0"],
+    stateTransitions: [
+      {
+        state: "Q0",
+        transitions: [
+          {
+            char: "a",
+            state: "Q0",
+          },
+        ],
+      },
+    ],
+  };
+
+  it("parses a simple DFA", () => {
+    const dfa = stripIndent`
+      alphabet: {a}
+      start: Q0
+      accepting: {Q0}
+      
+      Q0 (a -> Q0) 
+    `;
+
+    expect(parseDFA(dfa)).toStrictEqual(expected);
+  });
+});
